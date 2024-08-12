1ac1f62925bcb9d205871ce1a41e609be78b8792
lee0su <leeys4903@naver.com>
2024-08-12 19:25:09 +0900

    Sign up 조건 추가

--- core/src/main/resources/static/js/validation.js
+++ core/src/main/resources/static/js/validation.js
@@ -10,8 +10,27 @@ document.addEventListener("DOMContentLoaded", () => {
         const name = form.name.value;
         const birthdate = form.birthdate.value;
 
+        const usernameRegex = /^[a-zA-Z0-9]+$/;
+        const passwordRegex = /^[a-zA-Z0-9!@#$%^&*()_+]+$/;
+        const nameRegex = /^[a-zA-Z가-힣]+$/;
+
+        if (username.length < 6 || username.length > 12 || !usernameRegex.test(username)) {
+            alert('아이디는 6~12글자 사이여야 하며, 영어와 숫자만 사용이 가능합니다.');
+            return;
+        }
+
+        if (password.length < 8 || password.length > 16 || !/[a-zA-Z]/.test(password) || !/[0-9]/.test(password) || !passwordRegex.test(password)) {
+            alert('비밀번호는 8~16글자 사이여야 하며, 영어와 숫자, 특수기호를 포함해야 합니다.');
+            return;
+        }
+
         if (password !== confirmPassword) {
             alert('비밀번호가 일치하지 않습니다.');
+            return;
+        }
+
+        if (!nameRegex.test(name) || name.length < 2 || name.length > 10) {
+            alert('이름은 2~10글자 사이여야 하며, 영어와 한글만 사용이 가능합니다.');
             return;
         }
 
@@ -31,6 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
             });
 
             if (registerResponse.redirected) {
+                alert('회원가입이 완료되었습니다 !')
                 window.location.href = registerResponse.url;
             } else if (registerResponse.status === 409) {
                 alert('이미 사용중인 아이디입니다.');

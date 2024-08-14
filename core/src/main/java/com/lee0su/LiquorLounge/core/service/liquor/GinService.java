7f8c29ac34b249d5018bc4c89bb74dbc8b48bbc5
lee0su <leeys4903@naver.com>
2024-08-14 21:23:16 +0900

    안쓰는 import 삭제

--- core/src/main/java/com/lee0su/LiquorLounge/core/service/liquor/GinService.java
+++ core/src/main/java/com/lee0su/LiquorLounge/core/service/liquor/GinService.java
@@ -1,23 +1,17 @@
 package com.lee0su.LiquorLounge.core.service.liquor;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.lee0su.LiquorLounge.core.dto.liquor.GinDTO;
-import com.lee0su.LiquorLounge.core.dto.liquor.WhiskeyDTO;
 import com.lee0su.LiquorLounge.core.entity.liquor.GinEntity;
-import com.lee0su.LiquorLounge.core.entity.liquor.WhiskeyEntity;
 import com.lee0su.LiquorLounge.core.repository.liquor.GinRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class GinService {

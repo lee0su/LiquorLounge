546076dfaf27df6d0cf227453fdcd7492c9d9835
cordelia <137859418+farmercodel@users.noreply.github.com>
2024-09-30 00:31:32 +0900

    게시글 목록 오류 해결

--- core/src/main/java/com/lee0su/LiquorLounge/core/controller/web/PagesController.java
+++ core/src/main/java/com/lee0su/LiquorLounge/core/controller/web/PagesController.java
@@ -1,16 +1,23 @@
 package com.lee0su.LiquorLounge.core.controller.web;
 
+import com.lee0su.LiquorLounge.core.entity.community.Post;
 import com.lee0su.LiquorLounge.core.entity.user.UserEntity;
+import com.lee0su.LiquorLounge.core.service.community.PostService;
 import jakarta.servlet.http.HttpSession;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/pages")
 @Controller
 public class PagesController {
+    @Autowired
+    private PostService postService;
 
     @GetMapping("/main")
     public String mainPage() {
@@ -33,7 +40,10 @@ public class PagesController {
     }
 
     @GetMapping("/community")
-    public String communityPage() {
+    public String communityPage(Model model) {
+        // Model을 사용하면 컨트롤러에서 서비스나 데이터베이스로부터 가져온 데이터를 뷰(템플릿)로 쉽게 전송 가능
+        List<Post> posts = postService.getAllPosts(); // 게시글 목록 가져오기
+        model.addAttribute("posts", posts); // 모델에 게시글 리스트 추가
         return "pages/community";
     }
 
